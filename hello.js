d388a1dbadd078a1b9df379cc4dbca646c5d1da8
Lee Chun On <jeffreyleeon@gmail.com>
2017-12-10 10:25:54 -0800

    Improve by appending child to dom with id

--- hello.js
+++ hello.js
@@ -4,5 +4,10 @@ function sayHi() {
     var newHeader = goog.dom.createDom('h1', {
         'style': 'background-color:#EEE'
     }, 'Hello world111!');
-    goog.dom.appendChild(document.body, newHeader);
+    goog.dom.appendChild(goog.dom.$('header'), newHeader);
+}
+
+function sayBye() {
+    var newFooter = goog.dom.createDom('h3', {'style': 'background-color:#EEE'}, 'bye world!');
+    goog.dom.appendChild(goog.dom.$('footer'), newFooter);
 }
